8f0802d3058aa1e1032ed755efb36bf887cb6b66
simonangel-fong <simonangelfong@gmail.com>
2026-07-05 19:11:28 -0400

    argocd: albc add

--- infra/aws/03-providers.tf
+++ infra/aws/03-providers.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 3.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
   }
   backend "s3" {}
 }
